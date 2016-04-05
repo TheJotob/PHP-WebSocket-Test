4092041f8ddcc10e17bc5d8dacd49989733919dd
Joachim Eckerlin <joachim.eckerlin@gmail.com>
2016-04-05 23:05:05 +0200

    Moved functionality into socket_helper.php to keep socket.php more readable

--- socket.php
+++ socket.php
@@ -1,14 +1,14 @@
 <?php
 require_once('config.php');	// including configuration
-$host = Config::HOST; 			// reading socket host from config
-$port = Config::PORT; 			// reading socket port from config
+require_once('socket_helper.php');
 $null = NULL; 							// setting null var
 $warning = mask("WARNING");	// defining warning message
+$thresholds = array();
 
 // Setup Socket to listen on defined host and port
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($socket, $host, $port);
+socket_bind($socket, Config::HOST, Config::PORT);
 socket_listen($socket);
 $clients = array($socket);
 
@@ -24,7 +24,7 @@ while (true) {
 		$socket_new = socket_accept($socket);
 		$clients[] = $socket_new;
 		$header = socket_read($socket_new, 1024);
-		perform_handshaking($header, $socket_new, $host, $port);
+		perform_handshaking($header, $socket_new, Config::HOST, Config::PORT);
 		$found_socket = array_search($socket, $changed);
 		unset($changed[$found_socket]);
 	}
@@ -50,79 +50,6 @@ while (true) {
 	}
 }
 socket_close($socket);
-
-/**
-	* Decode message from client
-	* @param String $text Text to unmask
-  * @return String unmasked text
-	*/
-function unmask($text) {
-	$length = ord($text[1]) & 127;
-	if($length == 126) {
-		$masks = substr($text, 4, 4);
-		$data = substr($text, 8);
-	} elseif($length == 127) {
-		$masks = substr($text, 10, 4);
-		$data = substr($text, 14);
-	} else {
-		$masks = substr($text, 2, 4);
-		$data = substr($text, 6);
-	}
-	$text = "";
-	for ($i = 0; $i < strlen($data); ++$i) {
-		$text .= $data[$i] ^ $masks[$i%4];
-	}
-	return $text;
-}
-
-/**
-	* Encode message for transfer to client
-	* @param String $text Text to mask
-  * @return String masked text
-	*/
-function mask($text) {
-	$b1 = 0x80 | (0x1 & 0x0f);
-	// $b1 = 0x81;
-	$length = strlen($text);
-
-	if($length <= 125)
-		$header = pack('CC', $b1, $length);
-	elseif($length > 125 && $length < 65536)
-		$header = pack('CCn', $b1, 126, $length);
-	elseif($length >= 65536)
-		$header = pack('CCNN', $b1, 127, $length);
-	return $header.$text;
-}
-
-/**
-	* Handshake new client
-	* @param String $received_header Header received from client
-	* @param resource $client_conn Connection to client
-	* @param String $host Host address of socket
-	* @param Integer $port Port of socket
-  * @return float[] or false
-	*/
-function perform_handshaking($received_header,$client_conn, $host, $port) {
-	$headers = array();
-	$lines = preg_split("/\r\n/", $received_header);
-	foreach($lines as $line) {
-		$line = chop($line);
-		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches)) {
-			$headers[$matches[1]] = $matches[2];
-		}
-	}
-
-	$secKey = $headers['Sec-WebSocket-Key'];
-	$secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-	//hand shaking header
-	$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-	"Upgrade: websocket\r\n" .
-	"Connection: Upgrade\r\n" .
-	"WebSocket-Origin: $host\r\n" .
-	"WebSocket-Location: ws://$host:$port/socket.php\r\n".
-	"Sec-WebSocket-Accept:$secAccept\r\n\r\n";
-	socket_write($client_conn,$upgrade,strlen($upgrade));
-}
 
 /**
 	* Parse the acceleration data.
